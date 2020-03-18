d713eefa712bf7145e37fdde962c61e59b344418
穴澤恵理 <smoooooch701@yahoo.co.jp>
2020-03-18 06:22:15 +0000

    routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('XXX', 'Admin\AAAController@add');
+    
+});
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('admin/profile/create', 'Admin\ProfileController@add');
+    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+});
+
